38dee93aa8643b19d83cc6038adedd9db3527300
Brittany Cho <bac411@nyu.edu>
2020-01-10 23:02:01 -0500

    added login

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -8,6 +8,9 @@ class Home extends Component {
         <div>
           <img src="https://letstalkpayments.com/wp-content/uploads/2016/04/Bank.png" alt="bank"/>
           <h1>Bank of React</h1>
+
+          <Link to="/login">Log in</Link>
+          <br/>
           <Link to="/userProfile">User Profile</Link>
           <br/>
           <Link to="/debits">Debits</Link>
